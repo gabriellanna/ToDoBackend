8d270f9e8094e5c1ec9c9eec7fff7719fbc46ebd
Gabriel Lanna <alveslanna97@gmail.com>
2023-04-24 15:49:11 -0300

    Alterando ConnectionStrings

--- ToDo.Api/Controllers/TarefaController.cs
+++ ToDo.Api/Controllers/TarefaController.cs
@@ -24,7 +24,37 @@ namespace ToDo.Api.Controllers
             }
             catch (Exception ex)
             {
-                
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Tarefa>> Atualiazar(Tarefa tarefa)
+        {
+            try
+            {
+                var atualizarTarefaDb = await _service.Atualizar(tarefa);
+                return Ok(atualizarTarefaDb);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Tarefa>>> GetAll()
+        {
+            try
+            {
+                var getAllTarefaDb = await _service.PegarTodas();
+                return Ok(getAllTarefaDb);
+            }
+            catch (Exception ex)
+            {
+
                 return BadRequest(ex.Message);
             }
         }
